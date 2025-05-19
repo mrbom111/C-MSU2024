3bbffc8b3914b5a23c0f7eee7dc7c1c28471edbe
mrbom111 <67948839+mrbom111@users.noreply.github.com>
2025-05-19 21:33:58 +0300

    Update task5-8.py

--- sem2/py5/task5-8.py
+++ sem2/py5/task5-8.py
@@ -2,29 +2,26 @@ def compare_with_first(filename):
     file = open(filename, 'r')
 
     first = None
-    greater = 0
-    less = 0
+    numbers = []
 
     for line in file:
         parts = line.split()
         for word in parts:
             number = float(word)  # Преобразуем слово в число типа float (double)
-            
-            if first is None:
-                first = number
-            else:
-                if number > first:
-                    greater += 1
-                elif number < first:
-                    less += 1
+            numbers.append(number)
 
     file.close()
 
-    if first is None:
+    if not numbers:
         print("the file is empty.")
-    elif greater > less:
+        return
+
+    first = numbers[0]
+    counter = sum((1 if x > first else -1 if x < first else 0) for x in numbers[1:])
+
+    if counter > 0: 
         print("There are more elements greater than the first one.")
-    elif less > greater:
+    elif counter < 0: 
         print("There are more elements less than the first one.")
     else:
         print("There are the same amount of elements greater and less than the first one.")
