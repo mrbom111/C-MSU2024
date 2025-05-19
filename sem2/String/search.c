c97b96f049ed60f267ff1529e80ea79cce435705
mrbom111 <67948839+mrbom111@users.noreply.github.com>
2025-05-19 21:46:46 +0300

    Update search.c

--- sem2/String/search.c
+++ sem2/String/search.c
@@ -2,45 +2,44 @@
 
 int search(const char *SInputFile, char *word) {
     FILE *f = fopen(SInputFile, "r");
-    if (f==NULL) {
-        return -1; 
+    if (f == NULL) {
+        return -1;
     }
-	int linecurrlength=0, minlen = -1, flag=0;
 
-    char line[513], c; // 512 символов + '\0'
-    char minword[513] = ""; 
+    int linecurrlength = 0, minlen = -1, flag = 0;
+    int c;
+    char line[513]; //512 символов + '\0'
+    char minword[513] = "";
 
-
-	while ((c = fgetc(f)) != EOF){ //чтение по символам
-		if (c == '\n' || c == ' ' || c == '.' || c == ',') {
-    line[linecurrlength] = '\0';  
-    if (linecurrlength != 0 && line[0] == 'A') {
-        flag = 1;
-        for (int i = 0; i < linecurrlength; i++) {
-            if (!isupper((unsigned char)line[i])) {
-                flag = 0;
-                break;
+    while ((c = fgetc(f)) != EOF) { //чтение по символам
+        if (c == '\n' || c == '\r' || c == ' ' || c == '.' || c == ',') {
+            line[linecurrlength] = '\0';
+            if (linecurrlength != 0 && line[0] == 'A') {
+                flag = 1;
+                for (int i = 0; i < linecurrlength; i++) {
+                    if (!isupper((unsigned char)line[i])) {
+                        flag = 0;
+                        break;
+                    }
+                }
+                if (flag && (minlen == -1 || linecurrlength < minlen)) {
+                    strcpy(minword, line);
+                    minlen = linecurrlength;
+                }
             }
-        }
-        if (flag && (minlen == -1 || linecurrlength < minlen)) {
-            strcpy(minword, line);
-            minlen = linecurrlength;
+            linecurrlength = 0;
+        } else {
+            line[linecurrlength] = (char)c;
+            linecurrlength++;
         }
     }
-    linecurrlength = 0;
-	} else {
-		line[linecurrlength] = c;
-		linecurrlength++;
-	}
-}
 
     fclose(f);
 
     if (minlen != -1) {
         strcpy(word, minword);
+        return minlen;
     } else {
-        word[0] = '\0'; // если слово не найдено
+        return 0;
     }
-
-    return 1;
 }
